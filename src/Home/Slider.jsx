b688bd1d5f9b25bc3315d29a609728506d9bdc82
ikbal hossain asif <asifikbalgfh312@gamil.com>
2023-06-07 17:40:49 +0600

    home page slider create

--- src/Home/Slider.jsx
+++ src/Home/Slider.jsx
@@ -2,90 +2,75 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Parallax, Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from "swiper";
+import slide01 from '../assets/Images/slide01.jpg'
+import slide02 from '../assets/Images/slide02.jpg'
+import slide03 from '../assets/Images/slide03.jpg'
+import slide04 from '../assets/Images/slide04.jpg'
+import '../Css/Slider.css'
+
 
 const Slider = () => {
-    return (
-        <>
+  return (
+    <div>
       <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
         }}
-        speed={600}
-        parallax={true}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Parallax, Pagination, Navigation]}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <div
-          slot="container-start"
-          className="parallax-bg"
-          data-swiper-parallax="-23%"
-        ></div>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 1
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+          <div>
+            <span className="w-full h-full justify-center items-start flex-col bgColor absolute z-50 flex  md:p-24 p-16">
+              <h1 className="md:text-5xl text-2xl font-bold text-white">Discovering the Art and <br />Science of Photography <br />at our Summer Camp</h1>
+              <p className="text-base md:my-5 my-2 text-white">Discover your love for photography and immerse yourself <br />in a dynamic summer camp experience that will ignite <br />your creativity and expand your technical <br />skills behind the lens.</p>
+              <button className="btn bg-green-500 text-white hover:text-green-500 border-none">Explore More</button>
+            </span>
+            <img className="w-full relative" src={slide01} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 2
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+          <div>
+            <span className="w-full h-full justify-center items-start flex-col bgColor absolute z-50 flex  md:p-24 p-16">
+              <h1 className="md:text-5xl text-2xl font-bold text-white">Exploring Photography <br />Through Nature and Culture</h1>
+              <p className="text-base md:my-5 my-2 text-white">Explore Nature and Culture Through Photography": Embark on an exciting <br />photography adventure where you'll capture the beauty of nature and <br />immerse yourself in different cultures, guided by seasoned <br />photographers and surrounded by fellow enthusiasts.</p>
+              <button className="btn btn-sm md:btn-lg bg-green-500 text-white hover:text-green-500 border-none">Explore More</button>
+            </span>
+            <img className="w-full relative" src={slide02} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 3
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+          <div>
+            <span className="w-full h-full justify-center items-start flex-col bgColor absolute z-50 flex  md:p-24 p-16">
+              <h1 className="md:text-5xl text-2xl font-bold text-white">Immersive Photography <br />Journey at Summer Camp</h1>
+              <p className="text-base md:my-5 my-2 text-white">Join our intensive bootcamp and gain a comprehensive understanding of <br />photography, from camera basics to advanced techniques, empowering <br />you to capture stunning images and unleash your artistic vision.</p>
+              <button className="btn btn-sm md:btn-lg bg-green-500 text-white hover:text-green-500 border-none">Explore More</button>
+            </span>
+            <img className="w-full relative" src={slide03} alt="" />
           </div>
         </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <span className="w-full h-full justify-center items-start flex-col bgColor absolute z-50 flex  md:p-24 p-16">
+              <h1 className="md:text-5xl text-2xl font-bold text-white">A Summer Camp for <br />Aspiring Shutterbugs</h1>
+              <p className="text-base md:my-5 my-2 text-white">Develop your storytelling skills through the lens as you learn to <br/>communicate powerful narratives with your photographs <br/>guided by industry professionals in a supportive and <br/>inspiring summer camp environment.</p>
+              <button className="btn btn-sm md:btn-lg bg-green-500 text-white hover:text-green-500 border-none">Explore More</button>
+            </span>
+            <img className="w-full relative" src={slide04} alt="" />
+          </div>
+        </SwiperSlide>
+
       </Swiper>
-    </>
-    );
+    </div>
+  );
 };
 
 export default Slider;
