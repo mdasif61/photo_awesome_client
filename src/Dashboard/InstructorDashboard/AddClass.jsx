bc91cc90f86bf33c6ef2f996fda07a1f95f8eb89
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-07 21:27:16 +0600

    use form

--- src/Dashboard/InstructorDashboard/AddClass.jsx
+++ src/Dashboard/InstructorDashboard/AddClass.jsx
@@ -12,64 +12,89 @@ const AddClass = () => {
 
   return (
     <div className="w-8/12 mx-auto">
-      <form className="w-full">
-        <h1 className="text-2xl font-bold">Add Your Class</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <h1 className="text-2xl font-bold mb-10 uppercase text-center">
+          Add Your Class
+        </h1>
         <div className="grid grid-cols-1 w-full md:grid-cols-2 gap-5">
           <div className="w-full">
-            <label htmlFor="className">Class Name</label>
+            <label htmlFor="className">
+              <span className="font-bold">Class Name</span>
+            </label>
             <br />
             <input
-              className="w-full focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
+              className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
               type="text"
               id=""
+              placeholder="Enter Your Class Name"
+              {...register('className',{required:true})}
             />
           </div>
 
           <div className="w-full">
-            <label htmlFor="instructorName">Instructor Name</label>
+            <label htmlFor="instructorName">
+              <span className="font-bold">Instructor Name</span>
+            </label>
             <br />
             <input
-              className="w-full focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
+              className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
               type="text"
               id=""
+              placeholder="Instructor Name"
+              {...register('name')}
             />
           </div>
 
           <div className="w-full">
-            <label htmlFor="instructorEmail">Instructor Email</label>
+            <label htmlFor="instructorEmail">
+              <span className="font-bold">Instructor Email</span>
+            </label>
             <br />
             <input
-              className="w-full focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
-              type="text"
+              className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
+              type="email"
               id=""
+              placeholder="Instructor Email"
+              {...register('email')}
             />
           </div>
 
           <div className="w-full">
-            <label htmlFor="seat">Available Seats</label>
+            <label htmlFor="seat">
+              <span className="font-bold">Available Seats</span>
+            </label>
             <br />
             <input
-              className="w-full focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
+              className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
               type="number"
               id=""
+              placeholder="Enter Available Seats"
+              {...register('seats')}
             />
           </div>
 
           <div className="w-full">
-            <label htmlFor="price">Price</label>
+            <label htmlFor="price">
+              <span className="font-bold">Price</span>
+            </label>
             <br />
             <input
-              className="w-full focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
-              type="number"
+              className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
+              type="text"
               id=""
+              placeholder="Enter Price"
+              {...register('price')}
             />
           </div>
           <div className="w-full">
-            <label htmlFor="image">Image</label>
+            <label htmlFor="image">
+              <span className="font-bold">Image</span>
+            </label>
             <br />
             <input
               type="file"
-              className="file-input file-input-bordered file-input-success w-full"
+              className="file-input mt-2 h-12 rounded file-input-bordered file-input-success w-full"
+              {...register('photo')}
             />
           </div>
         </div>
