cb0aca0fb77a8fb64ea91e177ce4152913d4ace7
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-08 01:56:08 +0600

    update class

--- src/Dashboard/AdminDashboard/ManageClass.jsx
+++ src/Dashboard/AdminDashboard/ManageClass.jsx
@@ -1,26 +1,32 @@
+import axios from "axios";
 import useClasses from "../../hooks/useClasses";
 
 const ManageClass = () => {
   const { classes } = useClasses();
 
+  const handleApprove=(id)=>{
+    axios.patch(`http://localhost:5000/classes/${id}`,{status:'Approved'})
+  }
+
   return (
-    <div className="overflow-x-auto w-full p-10">
+    <div className="overflow-x-auto w-full p-5">
       <table className="table">
         {/* head */}
         <thead>
-          <tr>
+          <tr className="text-center font-bold text-lg text-black">
             <th>Image</th>
             <th>Name</th>
             <th>Instructor</th>
             <th>Instructor Email</th>
             <th>Available Seats</th>
             <th>Price</th>
+            <th>Status</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {classes.map((singleClass) => (
-            <tr key={singleClass._id}>
+            <tr className="text-center" key={singleClass._id}>
               <td>
                 <div className="avatar">
                   <div className="mask mask-squircle w-12 h-12">
@@ -35,11 +41,12 @@ const ManageClass = () => {
               <td>{singleClass.instructor}</td>
               <th>{singleClass.email}</th>
               <th>{singleClass.seats}</th>
-              <th>{singleClass.price}</th>
+              <th>${singleClass.price}</th>
+              <th>{singleClass.status}</th>
               <th>
-                <button className="btn btn-success btn-sm">Approved</button>
-                <button className="btn btn-warning btn-sm">Pending</button>
-                <button className="btn btn-error btn-sm">Denied</button>
+                <button onClick={()=>handleApprove(singleClass._id)} className="btn btn-success btn-sm">Approve</button>
+                <button className="btn btn-sm mx-2">Deny</button>
+                <button className="btn btn-sm">Send Feedback</button>
               </th>
             </tr>
           ))}
