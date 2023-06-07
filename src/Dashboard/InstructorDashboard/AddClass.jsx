0d3e13f566df67423ea507ea1420f4bd3d9ac97a
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-07 22:46:20 +0600

    get my class

--- src/Dashboard/InstructorDashboard/AddClass.jsx
+++ src/Dashboard/InstructorDashboard/AddClass.jsx
@@ -1,41 +1,52 @@
 import { useForm } from "react-hook-form";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Shared/Context";
 
-const image_token=import.meta.env.VITE_UPLOAD_IMAGE;
+const image_token = import.meta.env.VITE_UPLOAD_IMAGE;
 
 const AddClass = () => {
-    const [axiosSecure]=useAxiosSecure()
+  const [axiosSecure] = useAxiosSecure();
+  const {user}=useContext(AuthContext)
+  const [addLoading, setAddLoading] = useState(false);
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
-     const hosting_images_url=`https://api.imgbb.com/1/upload?key=${image_token}`
-
-     const formData=new FormData()
-     formData.append('image', data.photo[0]);
-     fetch(hosting_images_url,{
-        method:'POST',
-        body:formData
-     })
-     .then(res=>res.json())
-     .then(imageRes=>{
-        console.log(imageRes.success)
-        if(imageRes.success){
-            const imageURL=imageRes.data.display_url;
-            const {name,instructor,email,price,seats}=data;
-            const addClass={name,instructor,email, price:parseFloat(price), seats, image:imageURL, status:'Pending'}
-
-            axiosSecure.post('/classes', addClass)
-            .then(data=>{
-                console.log(data)
-                if(data.data.insertedId){
-                    alert('added success')
-                }
-            })
+    const hosting_images_url = `https://api.imgbb.com/1/upload?key=${image_token}`;
+    setAddLoading(true);
+    const formData = new FormData();
+    formData.append("image", data.photo[0]);
+    fetch(hosting_images_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imageRes) => {
+        console.log(imageRes.success);
+        if (imageRes.success) {
+          const imageURL = imageRes.data.display_url;
+          const { name, instructor, email, price, seats } = data;
+          const addClass = {
+            name,
+            instructor,
+            email,
+            price: parseFloat(price),
+            seats,
+            image: imageURL,
+            status: "Pending",
+          };
+          axiosSecure.post("/classes", addClass).then((data) => {
+            if (data.data.insertedId) {
+              Swal.fire("Successfull Added Class", "success");
+              setAddLoading(false);
+            }
+          });
         }
-     })
+      });
   };
 
   return (
@@ -55,7 +66,7 @@ const AddClass = () => {
               type="text"
               id=""
               placeholder="Enter Your Class Name"
-              {...register('name',{required:true})}
+              {...register("name", { required: true })}
             />
           </div>
 
@@ -68,8 +79,10 @@ const AddClass = () => {
               className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
               type="text"
               id=""
+              defaultValue={user?.displayName}
+              readOnly
               placeholder="Instructor Name"
-              {...register('instructor',{required:true})}
+              {...register("instructor", { required: true })}
             />
           </div>
 
@@ -82,8 +95,10 @@ const AddClass = () => {
               className="w-full mt-2 focus:outline-none h-12 py-2 px-4 border focus:bg-green-100 focus:border-b-2 border-green-600 rounded"
               type="email"
               id=""
+              defaultValue={user?.email}
+              readOnly
               placeholder="Instructor Email"
-              {...register('email')}
+              {...register("email")}
             />
           </div>
 
@@ -97,7 +112,7 @@ const AddClass = () => {
               type="number"
               id=""
               placeholder="Enter Available Seats"
-              {...register('seats')}
+              {...register("seats")}
             />
           </div>
 
@@ -111,7 +126,7 @@ const AddClass = () => {
               type="text"
               id=""
               placeholder="Enter Price"
-              {...register('price')}
+              {...register("price")}
             />
           </div>
           <div className="w-full">
@@ -122,14 +137,15 @@ const AddClass = () => {
             <input
               type="file"
               className="file-input mt-2 h-12 rounded file-input-bordered file-input-success w-full"
-              {...register('photo')}
+              {...register("photo")}
             />
           </div>
         </div>
         <input
+          disabled={addLoading}
           className="btn btn-block mt-7 hover:bg-green-500 bg-green-600 text-white"
           type="submit"
-          value="Add Class"
+          value={`${addLoading?'Class Adding...':'Add Class'}`}
         />
       </form>
     </div>
--- src/Dashboard/InstructorDashboard/MyClass.jsx
+++ src/Dashboard/InstructorDashboard/MyClass.jsx
@@ -1,5 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../../Shared/Context";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MyClass = () => {
+    const {user}=useContext(AuthContext)
+    const [axiosSecure]=useAxiosSecure()
+    const {data:myClass=[], refetch}=useQuery({
+        queryKey:['myclass', user?.email],
+        queryFn:async()=>{
+            const res=await axiosSecure.get(`/classes?email=${user?.email}`)
+            return res.data;
+        }
+        return []
+    })
+    console.log(myClass)
     return (
         <div>
             <h1>My Class</h1>
