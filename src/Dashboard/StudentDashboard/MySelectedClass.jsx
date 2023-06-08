4777f084bbad19f6c599c10f9132abd787af60f4
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-09 00:56:03 +0600

    select cart design

--- src/Dashboard/StudentDashboard/MySelectedClass.jsx
+++ src/Dashboard/StudentDashboard/MySelectedClass.jsx
@@ -1,28 +1,47 @@
+import { FaTrash } from "react-icons/fa";
 import useSelectedClass from "../../hooks/useSelectedClass";
 
 const MySelectedClass = () => {
+  const { selectClass, refetch } = useSelectedClass();
 
-    const {selectClass,refetch}=useSelectedClass()
-
-    return (
-        <div className="p-12">
-            <h1 className="text-xl font-bold">Total Select Class : {selectClass.length}</h1>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
-                {
-                    selectClass.map(select=>(
-                        <div key={select._id}>
-                            <div>
-                                <img src={select.image} alt="" />
-                            </div>
-                            <div>
-                                <h1>Class : {select.name}</h1>
-                            </div>
-                        </div>
-                    ))
-                }
+  return (
+    <div className="p-12">
+      <h1 className="text-xl font-bold mb-5 text-gray-600">
+        Total Select Class : {selectClass.length}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+        {selectClass.map((select) => (
+          <div
+            key={select._id}
+            className="p-5 border rounded-md flex flex-col justify-between"
+          >
+            <div className="bg-black relative rounded-md h-56 overflow-hidden">
+              <img className="h-full w-full" src={select.image} alt="" />
+              <button className="btn absolute top-5 border-none right-5 text-red-600 bg-white rounded-full shadow-lg btn-sm">
+                <FaTrash />
+              </button>
             </div>
-        </div>
-    );
+            <div>
+              <h1 className="text-xl text-gray-600 my-3 font-bold">
+                Class : {select.name}
+              </h1>
+              <h2 className="text-gray-600 font-semibold text-lg">
+                Price : ${select.price}
+              </h2>
+              <h2 className="text-gray-600">
+                Instructor : {select.instructor}
+              </h2>
+            </div>
+            <div>
+              <button className="btn bg-green-500 text-white text-lg btn-block">
+                Pay
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MySelectedClass;
