b6908d8019dba26e1bc2002e3a5d9c55396cc8e8
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-06-09 00:18:29 +0600

    select class post

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -3,15 +3,43 @@ import ClassesCart from "./ClassesCart";
 import Container from "../container/Container";
 import useApproveClass from "../hooks/useApproveClass";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import { useContext } from "react";
+import { AuthContext } from "../Shared/Context";
+import Swal from "sweetalert2";
 
 const Classes = () => {
   const { approvedClass } = useApproveClass();
   const [axiosSecure] = useAxiosSecure();
+  const { user } = useContext(AuthContext);
 
   const handleSelect = (singleClass) => {
-    const { name, instructor, price, status, seats, _id } = singleClass;
-    const selectClass={name, instructor, price, seats, selectId:_id}
-    // axiosSecure.post('/selectedClass')
+    const { name, instructor, price, seats, _id } = singleClass;
+    const selectClass = {
+      name,
+      instructor,
+      price,
+      seats,
+      selectId: _id,
+      email: user?.email,
+    };
+    axiosSecure.post("/selectedClass", selectClass).then((res) => {
+      if (res.data.message) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: res.data.message,
+        });
+      }
+      if (res.data.insertedId) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Select Success",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
 
   return (
